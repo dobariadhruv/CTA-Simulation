bffb2863f39f72fa36ae7b5602c9a8c2b3b3bbf7
inkWanderer <kwndnl@gmail.com>
2023-03-14 20:23:16 -0700

    Add files via upload

---
+++ FinalProject_ProjectCode.py
@@ -0,0 +1,444 @@
+import numpy as np
+import numpy.random as rand
+
+
+class MonteCarlo:
+    """
+    The SimulateOnce method is declared as abstract (it doesn't have an implementation
+    in the base class) that must be extended/overriden to build the simualtion.
+    """
+
+    def SimulateOnce(self):
+        raise NotImplementedError
+
+    def bootstrap(self, x, confidence=.68, nSamples=100):
+        # Make "nSamples" new datasets by re-sampling x with replacement
+        # the size of the samples should be the same as x itself
+        means = []
+        for k in range(nSamples):
+            sample = np.random.choice(x, size=len(x), replace=True)
+            means.append(np.mean(sample))
+        means.sort()
+        leftTail = int(((1.0 - confidence) / 2) * nSamples)
+        rightTail = (nSamples - 1) - leftTail
+        return means[leftTail], np.mean(x), means[rightTail]
+
+    # This function computes the value at risk amount for the results of a simulation
+    # It can only be run after the main simulation is run, so in this function we
+    # test for whether the results member exists and if not, print an error
+    def var(self, risk=.05):
+        if hasattr(self, "results"):  # See if the results have been calculated
+            self.results.sort()  # Sort them
+            index = int(len(self.results) * risk)  # Count them and multiply by the risk factor
+            return (self.results[index])  # Return the value at that index
+        else:
+            print("RunSimulation must be executed before the method 'var'")
+            return 0.0
+
+    # This function runs the simulation.  Note that it stores the results of each of the
+    # trials in an array that is a CLASS variable, not a local variable in this function
+    # so, we can get this array from the MonteCarlo object after running if we wish,
+    # and we can also call the "var" method as well
+    def RunSimulation(self, simCount=10000):
+        self.results = []  # Array to hold the results
+
+        # Now, we set up the simulation loop
+        self.convergence = False
+        for k in range(simCount):
+            x = self.SimulateOnce()  # Run the simulation
+            self.results.append(x)  # Add the result to the array
+
+        return self.bootstrap(self.results)
+
+        
+combined_stations = np.array([("Addison-O'Hare", 2455, 917),
+    ("Austin-Forest Park", 1629, 605),
+    ("Belmont-O'Hare", 4532, 1447),
+    ("California/Milwaukee", 4609, 1421),
+    ("Chicago/Milwaukee", 3469, 1269),
+    ("Cicero-Forest Park", 1220, 307),
+    ("Clark/Lake", 16359, 7331),
+    ("Clinton-Forest Park", 3020, 1196),
+    ("Cumberland", 3750, 1387),
+    ("Damen/Milwaukee", 5865, 1463),
+    ("Division/Milwaukee", 5153, 1749),
+    ("Forest Park", 2854, 998),
+    ("Grand/Milwaukee", 2375, 722),
+    ("Harlem-Forest Park", 1020, 348),
+    ("Medical Center", 2278, 1134),
+    ("Irving Park-O'Hare", 3694, 1130),
+    ("Jackson/Dearborn", 6026, 2466),
+    ("Jefferson Park", 5647, 1807),
+    ("Kedzie-Homan-Forest Park", 1835, 489),
+    ("LaSalle", 2529, 1002),
+    ("Logan Square", 6271, 1859),
+    ("Monroe/Dearborn", 6310, 2868),
+    ("Montrose-O'Hare", 2141, 767),
+    ("O'Hare Airport", 10832, 2082),
+    ("Oak Park-Forest Park", 1466, 621),
+    ("Pulaski-Forest Park", 1615, 297),
+    ("Racine", 1843, 772),
+    ("Rosemont", 5598, 1685),
+    ("UIC-Halsted", 4686, 2737),
+    ("Washington/Dearborn", 10718, 3669),
+    ("Western-Forest Park", 1421, 424),
+    ("Western/Milwaukee", 4496, 1564)])
+
+
+weekday_stations = np.array([("Addison-O'Hare", 2949, 519),
+    ("Austin-Forest Park", 1966, 320),
+    ("Belmont-O'Hare", 5292, 824),
+    ("California/Milwaukee", 5366, 822),
+    ("Chicago/Milwaukee", 4174, 673),
+    ("Cicero-Forest Park", 1377, 192),
+    ("Clark/Lake", 20578, 3469),
+    ("Clinton-Forest Park", 3704, 580),
+    ("Cumberland", 4499, 739),
+    ("Damen/Milwaukee", 6531, 984),
+    ("Division/Milwaukee", 6084, 1025),
+    ("Forest Park", 3403, 535),
+    ("Grand/Milwaukee", 2755, 426),
+    ("Harlem-Forest Park", 1209, 192),
+    ("Medical Center", 2851, 789),
+    ("Irving Park-O'Hare", 4316, 608),
+    ("Jackson/Dearborn", 7337, 1430),
+    ("Jefferson Park", 6650, 937),
+    ("Kedzie-Homan-Forest Park", 2073, 337),
+    ("LaSalle", 3058, 599),
+    ("Logan Square", 7280, 1015),
+    ("Monroe/Dearborn", 7963, 1349),
+    ("Montrose-O'Hare", 2560, 431),
+    ("O'Hare Airport", 11320, 1990),
+    ("Oak Park-Forest Park", 1818, 309),
+    ("Pulaski-Forest Park", 1754, 206),
+    ("Racine", 2226, 534),
+    ("Rosemont", 6388, 1098),
+    ("UIC-Halsted", 5849, 2357),
+    ("Washington/Dearborn", 12736, 1937),
+    ("Western-Forest Park", 1639, 270),
+    ("Western/Milwaukee", 5370, 810)])
+
+
+weekend_stations = np.array([("Addison-O'Hare", 1221, 363),
+    ("Austin-Forest Park", 787, 185),
+    ("Belmont-O'Hare", 2633, 759),
+    ("California/Milwaukee", 2718, 608),
+    ("Chicago/Milwaukee", 1707, 386),
+    ("Cicero-Forest Park", 828, 152),
+    ("Clark/Lake", 5820, 1534),
+    ("Clinton-Forest Park", 1311, 263),
+    ("Cumberland", 1879, 683),
+    ("Damen/Milwaukee", 4203, 1094),
+    ("Division/Milwaukee", 2829, 717),
+    ("Forest Park", 1480, 360),
+    ("Grand/Milwaukee", 1425, 330),
+    ("Harlem-Forest Park", 549, 140),
+    ("Medical Center", 847, 281),
+    ("Irving Park-O'Hare", 2139, 395),
+    ("Jackson/Dearborn", 2750, 1068),
+    ("Jefferson Park", 3141, 651),
+    ("Kedzie-Homan-Forest Park", 1242, 244),
+    ("LaSalle", 1207, 413),
+    ("Logan Square", 3751, 781),
+    ("Monroe/Dearborn", 2182, 592),
+    ("Montrose-O'Hare", 1094, 241),
+    ("O'Hare Airport", 9612, 1785),
+    ("Oak Park-Forest Park", 588, 168),
+    ("Pulaski-Forest Park", 1268, 181),
+    ("Racine", 884, 296),
+    ("Rosemont", 3625, 1213),
+    ("UIC-Halsted", 1781, 718),
+    ("Washington/Dearborn", 5675, 1442),
+    ("Western-Forest Park", 876, 180),
+    ("Western/Milwaukee", 2312, 485)])
+
+
+
+
+class TrainSimulation(MonteCarlo):
+    def __init__(self, stations, num_trains, weather, big_events):
+        self.stations = stations
+        self.num_trains = num_trains
+        self.weather = weather
+        self.big_events = big_events
+
+
+    def SimulateOneTrain(self):
+        # Generating riders based on station distributions
+        riders_in_stations = []
+
+        for station in self.stations:
+            avg, s_dev = int(station[1]), int(station[2])
+            # Create number of riders in the day
+            daily_num = rand.normal(avg, s_dev)
+            
+            if self.weather == 'inclement':
+                daily_num *= 0.8
+            elif self.weather == 'sunny':
+                daily_num *= 1.5
+            
+            # Adjust riders for big events
+            if self.big_events == 'True':
+                daily_num *= 1.6
+            
+            
+            # Divide by number of trains that are sent through the day
+            riders_in_stations.append(int(daily_num / self.num_trains))
+
+        # Setting station riders to a minimum of 0, to avoid negative riders on a platform
+        for i in range(len(riders_in_stations)):
+            if riders_in_stations[i] <= 0:
+                riders_in_stations[i] = 0
+
+        # Tracking Destinations
+        rider_destinations = {}
+        for station in self.stations:
+            rider_destinations[station[0]] = 0
+
+        # Tracking people on the train
+        people_on_train = 0
+
+        # Iterate through stations
+        # From O'Hare to Forest Park
+        for i in range(len(self.stations)):
+
+            # Remove people getting off the train at this destination
+            people_on_train -= rider_destinations[self.stations[i][0]]
+            rider_destinations[self.stations[i][0]] = 0
+
+            # Add new riders to train
+            new_passengers = riders_in_stations[i]
+            people_on_train += new_passengers
+            riders_in_stations[i] = 0
+
+            # If the train is at capacity, we can't have everyone on board
+            if people_on_train > 640:
+                overflow = people_on_train - 640
+                new_passengers -= overflow
+                riders_in_stations[i] += overflow
+                people_on_train = 640
+
+            # Distribute new riders to all possible destinations
+
+            # Figure out possible destinations for new riders
+            possible_destinations = len(self.stations[i:])
+            index_start = len(rider_destinations) - possible_destinations - 1
+            destination_index = index_start
+
+            while new_passengers > 0:
+                station_name = self.stations[destination_index][0]
+                rider_destinations[station_name] += 1
+                new_passengers -= 1
+                destination_index += 1
+
+                if destination_index == len(rider_destinations) - 1:
+                    destination_index = index_start
+
+        # print(rider_destinations)
+
+        return sum(riders_in_stations)
+
+
+    def SimulateOneTrainReverse(self):
+        # Generating riders based on station distributions
+        riders_in_stations = []
+
+        for station in self.stations:
+            avg, s_dev = int(station[1]), int(station[2])
+            # Create number of riders in the day
+            daily_num = rand.normal(avg, s_dev)
+            # Divide by number of trains that are sent through the day
+            riders_in_stations.append(int(daily_num / self.num_trains))
+
+
+        # Setting station riders to a minimum of 0, to avoid negative riders on a platform
+        for i in range(len(riders_in_stations)):
+            if riders_in_stations[i] <= 0:
+                riders_in_stations[i] = 0
+
+        # Tracking Destinations
+        rider_destinations = {}
+        for station in self.stations:
+            rider_destinations[station[0]] = 0
+
+        # Tracking people on the train
+        people_on_train = 0
+
+        # Iterate through stations
+        # From O'Hare to Forest Park
+        for i in range(len(self.stations) - 1, -1, -1):
+
+            # Remove people getting off the train at this destination
+            people_on_train -= rider_destinations[self.stations[i][0]]
+            rider_destinations[self.stations[i][0]] = 0
+
+            # Add new riders to train
+            people_on_train += riders_in_stations[i]
+            new_passengers = riders_in_stations[i]
+            riders_in_stations[i] = 0
+
+            # If the train is at capacity, we can't have everyone on board
+            if people_on_train > 640:
+                overflow = people_on_train - 640
+                new_passengers -= overflow
+                riders_in_stations[i] += overflow
+                people_on_train = 640
+
+            # Distribute new riders to all possible destinations
+
+            # Figure out possible destinations for new riders
+            index_start = i - 1
+            destination_index = index_start
+
+            while new_passengers > 0:
+                station_name = self.stations[destination_index][0]
+                rider_destinations[station_name] += 1
+                new_passengers -= 1
+                destination_index -= 1
+
+                if destination_index <= 0:
+                    destination_index = index_start
+
+        return sum(riders_in_stations)
+
+
+    def SimulateOnce(self):
+        riders = 0
+        reverse = 0
+        for i in range(self.num_trains):
+            riders += self.SimulateOneTrain()
+            reverse += self.SimulateOneTrainReverse()
+        return riders + reverse
+
+weather = np.random.choice(['inclement', 'sunny'])
+big_events = np.random.choice(['True', 'False'])
+
+sim1 = TrainSimulation(stations, 200, weather , big_events)
+print(sim1.SimulateOnce())
+sim1.RunSimulation(10)
+
+
+
+
+##### Commute traffic
+
+### SIM 1
+### Sunny weather,  weekday commute, no event, trains from 50 - 250 incremented by 25.
+
+numTrains = list(range(50, 251, 25))
+sim1_CI = []
+sim1_vars = []
+
+for trains in numTrains:
+    sim = TrainSimulation(weekday_stations, trains, 'sunny', 'False')
+    result = sim.RunSimulation(100)
+    var = sim.var(0.95)
+    sim1_CI.append(result)
+    sim1_vars.append(var)
+
+sim1_mean = np.array(sim1_CI)[:,1]
+
+
+### SIM 2
+### Sunny weather,  weekend, no event, trains from 50 - 250 incremented by 25.
+
+sim2_CI = []
+sim2_vars = []
+
+for trains in numTrains:
+    sim = TrainSimulation(weekend_stations, trains, 'sunny', 'False')
+    result = sim.RunSimulation(100)
+    var = sim.var(0.95)
+    sim2_CI.append(result)
+    sim2_vars.append(var)
+
+sim2_mean = np.array(sim2_CI)[:,1]
+
+### SIM 3
+### Cold weather,  weekday commute, no event, trains from 50 - 250 incremented by 25.
+
+numTrains = list(range(50, 251, 25))
+sim3_CI = []
+sim3_vars = []
+
+for trains in numTrains:
+    sim = TrainSimulation(weekday_stations, trains, 'inclement', 'False')
+    result = sim.RunSimulation(100)
+    var = sim.var(0.95)
+    sim3_CI.append(result)
+    sim3_vars.append(var)
+
+sim3_mean = np.array(sim3_CI)[:,1]
+
+### SIM 4
+### Cold weather,  weekend, no event, trains from 50 - 250 incremented by 25.
+
+sim4_CI = []
+sim4_vars = []
+
+for trains in numTrains:
+    sim = TrainSimulation(weekend_stations, trains, 'inclement', 'False')
+    result = sim.RunSimulation(100)
+    var = sim.var(0.95)
+    sim4_CI.append(result)
+    sim4_vars.append(var)
+
+sim4_mean = np.array(sim4_CI)[:,1]
+
+plt.plot(numTrains, sim1_mean, label = "Sunny Commute Mean")
+plt.plot(numTrains, sim2_mean, label = "Sunny Weekend Mean")
+plt.plot(numTrains, sim3_mean, label = "Inclement Commute Mean")
+plt.plot(numTrains, sim4_mean, label = "Inclement Weekend Mean")
+plt.xlabel("Number of trains in day")
+plt.ylabel("Number of riders unable to catch a train")
+plt.title("Mean Riders Compared over Commute and Weather")
+plt.legend()
+plt.show()
+
+
+##### Outlier Events
+
+### Sunny, weekday, big event, trains from 50-350 incremented by 25
+
+numTrains2 = list(range(50, 351, 25))
+sim5_CI = []
+sim5_vars = []
+
+for trains in numTrains2:
+    sim = TrainSimulation(weekday_stations, trains, 'sunny', 'True')
+    result = sim.RunSimulation(100)
+    var = sim.var(0.95)
+    sim5_CI.append(result)
+    sim5_vars.append(var)
+
+plt.plot(numTrains2, sim5_vars, label = "High-End Outlier")
+plt.plot(numTrains, sim1_vars, label = "Sunny Commute")
+plt.legend()
+plt.title("VAR for Regular Commute vs High-End Outlier")
+plt.xlabel("Number of trains in day")
+plt.ylabel("Number of riders unable to catch a train")
+plt.show()
+
+
+
+## Inclement weekend VAR
+
+numTrains3 = list(range(5, 76, 5))
+sim6_CI = []
+sim6_vars = []
+
+for trains in numTrains3:
+    sim = TrainSimulation(weekend_stations, trains, 'inclement', 'False')
+    result = sim.RunSimulation(100)
+    var = sim.var(0.05)
+    sim6_CI.append(result)
+    sim6_vars.append(var)
+
+
+plt.plot(numTrains3, sim6_vars, label = "Low-End Outlier")
+plt.plot(numTrains, sim1_vars, label = "Sunny Commute")
+plt.legend()
+plt.title("VAR (5%) for Regular Commute vs Low-End Outlier")
+plt.xlabel("Number of trains in day")
+plt.ylabel("Number of riders unable to catch a train")
+plt.show()
